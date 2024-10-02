79391b97ae47e43c242d8e1df9278f34d28d8a00
Ednardo Gomes <ednardogomesdev@gmail.com>
2024-10-02 11:06:09 -0300

    create UserEntity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+/* eslint-disable prettier/prettier */
+
+import { ObjectId } from 'mongodb';
+import { Column, Entity, ObjectIdColumn } from 'typeorm';
+
+@Entity()
+export class UserEntity {
+  @ObjectIdColumn()
+  _id: ObjectId
+
+  @Column()
+  name: string;
+
+  @Column()
+  surname: string;
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string;
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,9 +2,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
