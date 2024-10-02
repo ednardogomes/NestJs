8a94f269aa9d0646f63568ff9c6cf7cfcde10d6c
Ednardo Gomes <ednardogomesdev@gmail.com>
2024-10-02 10:26:34 -0300

    start of CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,4 +1,20 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-
-@Module({})
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_CONNECTION_STRING,
+      database: process.env.MONGODB_DATABASE,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+  ],
+})
 export class DbModule {}
