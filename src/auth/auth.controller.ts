3f6ec5cf55873904ae74e06107bd7b7dee45e3b2
Ednardo Gomes <ednardogomesdev@gmail.com>
2024-10-03 14:40:52 -0300

    feat: start of user authetication

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+}
