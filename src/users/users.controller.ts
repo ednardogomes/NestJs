ebe94307790f5ba267712ba6d2317aa4afaf07f1
Ednardo Gomes <ednardogomesdev@gmail.com>
2024-10-08 13:37:31 -0300

    refactor: UseGuards in all routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,34 +13,38 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { UserEntity } from './entities/user.entity';
 
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-  
+
   @Post()
-  async create(@Body() createUser: CreateUserDto) {
+  async create(@Body() createUser: CreateUserDto): Promise<string> {
     return await this.usersService.create(createUser);
   }
-  
-  @UseGuards(AuthGuard)
+
   @Get()
-  async findAll() {
+  async findAll(): Promise<UserEntity[]> {
     return await this.usersService.findAll();
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string): Promise<UserEntity> {
     return await this.usersService.findOne(id);
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateUser: UpdateUserDto) {
+  async update(
+    @Param('id') id: string,
+    @Body() updateUser: UpdateUserDto,
+  ): Promise<string> {
     return await this.usersService.update(id, updateUser);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string): Promise<string> {
     return await this.usersService.remove(id);
   }
 }
